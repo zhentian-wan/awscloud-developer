2638e27e85a493b8c6fe49575cbdf5eb95369bd0
Zhentian Wan <zhentian.wan@varian.com>
2021-03-22 16:44:43 +0200

    save

--- course-02/exercises/udacity-c2-basic-server/src/cars.ts
+++ course-02/exercises/udacity-c2-basic-server/src/cars.ts
@@ -1,14 +1,26 @@
 export interface Car {
-    make: string;
-    type: string;
-    model: string;
-    cost: number;
-    id: number;
+  make: string;
+  type: string;
+  model: string;
+  cost: number;
+  id: number;
 }
 
 export const cars: Car[] = [
-    { make: 'tesla', type: 'sedan', model: 'roadster', cost: 33, id: 0 },
-    { make: 'tesla', type: 'suv', model: 'model 3', cost: 48, id: 1 },
-    { make: 'toyota', type: 'sedan', model: 'prius', cost: 22, id: 2 },
-    { make: 'honda', type: 'sedan', model: 'civic', cost: 22, id: 3 }
-   ]
+  { make: "tesla", type: "sedan", model: "roadster", cost: 33, id: 0 },
+  { make: "tesla", type: "suv", model: "model 3", cost: 48, id: 1 },
+  { make: "toyota", type: "sedan", model: "prius", cost: 22, id: 2 },
+  { make: "honda", type: "sedan", model: "civic", cost: 22, id: 3 },
+];
+
+export const getCarById = (id: number): Car => {
+  const foundIdx = cars.findIndex((car) => car.id === id);
+  if (foundIdx === -1) {
+    return null;
+  }
+  return cars[foundIdx];
+};
+
+export const addCar = (car: Car) => {
+  cars.push(car);
+};
--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,19 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+  dev: {
+    username: process.env.UDACITY_PROJECT2_DEV_USERNAME,
+    password: process.env.UDACITY_PROJECT2_DEV_PASSWORD,
+    database: process.env.UDACITY_PROJECT2_DEV_DB,
+    host: process.env.UDACITY_PROJECT2_DEV_HOST,
+    dialect: process.env.UDACITY_PROJECT2_DEV_DIALECT,
+    aws_region: process.env.UDACITY_PROJECT2_DEV_AWS_REGION,
+    aws_profile: process.env.UDACITY_PROJECT2_DEV_AWS_PROFILE,
+    aws_media_bucket: process.env.UDACITY_PROJECT2_DEV_MEDIA_BUCKET,
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+  prod: {
+    username: "",
+    password: "",
+    database: process.env.UDACITY_PROJECT2_PROD_DB,
+    host: "",
+    dialect: process.env.UDACITY_PROJECT2_PROD_DIALECT,
+  },
+};
