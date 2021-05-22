13c4eecd0f827397f5610d72b2cc6bf9c762b1fd
Zhentian Wan <zhentian.wan@varian.com>
2021-05-22 10:53:30 +0300

    connect client to API and Auth0

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cloud-developer
+
 content for Udacity's cloud developer nanodegree
+
+## Auth
+
+course-04/lesson-6/client
--- course-04/exercises/lesson-6/client/src/config.ts
+++ course-04/exercises/lesson-6/client/src/config.ts
@@ -1,7 +1,8 @@
-export const apiEndpoint = '...'
+export const apiEndpoint =
+  "https://11k01yi7cf.execute-api.us-east-1.amazonaws.com/dev";
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
-  callbackUrl: 'http://localhost:3000/callback'
-}
+  domain: "dev-w7mouh1c.eu.auth0.com",
+  clientId: "jt4mGZJ6HcmUxDgkgzDz4VfGTzEVlLrl",
+  callbackUrl: "http://localhost:3000/callback",
+};
